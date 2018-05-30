bfd6f7adca6810ebfe9906db5146b9b3461885b3
Michael Liu <michaelliu56@gmail.com>
2018-05-30 02:57:54 -0400

    added files

---
+++ assets/javascript/app.js
@@ -0,0 +1,145 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyD1WdNYTW7UDBuGgV0MXM5HCT_exUgvOOo",
+    authDomain: "rich-boulevard-203215.firebaseapp.com",
+    databaseURL: "https://rich-boulevard-203215.firebaseio.com",
+    projectId: "rich-boulevard-203215",
+    storageBucket: "rich-boulevard-203215.appspot.com",
+    messagingSenderId: "369092792103"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
+var chatRef = database.ref('chats');
+var usersRef = database.ref('users/');
+var gamesRef = database.ref('games/');
+var staticsRef = database.ref('statics/');
+
+var name1 = "";
+var wins1 = 0;
+var losses1 = 0;
+var name2 = "";
+var wins2 = 0;
+var losses2 = 0;
+var choice1 = "";
+var choice2 = "";
+var userKey = "";
+var photo ="";
+var googleLogin = false;
+
+//Goggle Sign In function
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  var name = profile.getName();
+  var ID = profile.getId();
+  var email = profile.getEmail();
+  //store the photo URL
+  var photo = profile.getImageUrl();
+  googleLogin = true;
+  userKey = ID;
+  usersRef.child(userKey).update({uID:ID,name,name,photo:photo,email:email,status:"online"});
+}
+//Google Sign out function
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  var d = new Date();
+  var lastdisconnect = d.toUTCString();
+  auth2.signOut().then(function () {
+    usersRef.child(userKey).update({status:"offline",lastdisconnect:lastdisconnect});
+    console.log('User signed out.');
+  });
+}
+
+//Chat Room Function
+(function(){
+  
+  var chat = {
+    messageToSend: '',
+
+    init: function() {
+      this.cacheDOM();
+      this.bindEvents();
+      this.render();
+    },
+    cacheDOM: function() {
+      this.$chatHistory = $('.chat-history');
+      this.$button = $('button');
+      this.$textarea = $('#message-to-send');
+      this.$chatHistoryList =  this.$chatHistory.find('ul');
+    },
+    bindEvents: function() {
+      this.$button.on('click', this.addMessage.bind(this));
+      this.$textarea.on('keyup', this.addMessageEnter.bind(this));
+    },
+    render: function() {
+      this.scrollToBottom();
+      if (this.messageToSend.trim() !== '') {
+        var template = Handlebars.compile( $("#message-template").html());
+        var context = { 
+          messageOutput: this.messageToSend,
+          time: this.getCurrentTime()
+        };
+
+        this.$chatHistoryList.append(template(context));
+        this.scrollToBottom();
+        this.$textarea.val('');
+        
+        // responses
+        var templateResponse = Handlebars.compile( $("#message-response-template").html());
+        var contextResponse = { 
+          response: this.getRandomItem(this.messageResponses),
+          time: this.getCurrentTime()
+        };
+        
+        setTimeout(function() {
+          this.$chatHistoryList.append(templateResponse(contextResponse));
+          this.scrollToBottom();
+        }.bind(this), 1500);
+        
+      }
+      
+    },
+    
+    addMessage: function() {
+      this.messageToSend = this.$textarea.val()
+      this.render();         
+    },
+    addMessageEnter: function(event) {
+        // enter was pressed
+        if (event.keyCode === 13) {
+          this.addMessage();
+        }
+    },
+    scrollToBottom: function() {
+       this.$chatHistory.scrollTop(this.$chatHistory[0].scrollHeight);
+    },
+    getCurrentTime: function() {
+      return new Date().toLocaleTimeString().
+              replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3");
+    },
+    getRandomItem: function(arr) {
+      return arr[Math.floor(Math.random()*arr.length)];
+    }
+    
+  };
+  
+  chat.init();
+  
+  var searchFilter = {
+    options: { valueNames: ['name'] },
+    init: function() {
+      var userList = new List('people-list', this.options);
+      var noItems = $('<li id="no-items-found">No items found</li>');
+      
+      userList.on('updated', function(list) {
+        if (list.matchingItems.length === 0) {
+          $(list.list).append(noItems);
+        } else {
+          noItems.detach();
+        }
+      });
+    }
+  };
+  
+  searchFilter.init();
+  
+})();
---
+++ assets/javascript/coverflow.js
@@ -0,0 +1,43 @@
+/*------------------------------
+        Album Cover Slider
+--------------------------------*/
+
+
+function setupCoverflow(coverflowContainer) {
+  var coverflowContainers;
+
+  if (typeof coverflowContainer !== "undefined") {
+    if (Array.isArray(coverflowContainer)) {
+      coverflowContainers = coverflowContainer;
+    } else {
+      coverflowContainers = [coverflowContainer];
+    }
+  } else {
+    coverflowContainers = Array.prototype.slice.apply(document.getElementsByClassName('coverflow'));
+  }
+
+  coverflowContainers.forEach(function(containerElement) {
+    var coverflow = {};
+
+
+    //capture coverflow elements
+    coverflow.container = containerElement;
+    coverflow.images = Array.prototype.slice.apply(containerElement.getElementsByClassName('coverflow__image'));
+    coverflow.position = Math.floor(coverflow.images.length / 2) + 1;
+
+    //set indicies on images
+    coverflow.images.forEach(function(coverflowImage, i) {
+      coverflowImage.dataset.coverflowIndex = i + 1;
+    });
+
+    //set initial position
+    coverflow.container.dataset.coverflowPosition = coverflow.position;
+
+
+
+  });
+}
+
+setupCoverflow();
+
+
