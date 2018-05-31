d28d020917f6e05fd0ac6a4c0758bb9b8fd233a0
Michael Liu <michaelliu56@gmail.com>
2018-05-31 19:01:57 -0400

    modify files

---
+++ assets/javascript/cookie.js
@@ -0,0 +1,41 @@
+//Save the keywords into cookie
+// cname - the parameter name
+// cvalue - vlaue of the parameter
+// exdays - expiry date of the parameter i.e. 30 days
+function setCookie(cname,cvalue,exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+//Get the value from the cookie
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+//Delete the cookie by passing the parameter name
+function delCookie(cname) {
+    document.cookie = cname + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/';
+}
+//Clear all cookie 
+function deleteAllCookies() {
+    var cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/";
+    }
+}
