e55360c873d55b6014891ba34ccc7bc0e3f6fb1a
Michael Liu <michaelliu56@gmail.com>
2018-05-31 19:06:42 -0400

    modify files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -39,7 +39,7 @@ function onSignIn(googleUser) {
   $(".user-photo").html("<img class='rounded-circle' src="+ photo +" alt='avatar' />");
   $(".chat-with").text(name);
   setCookie("fbuID", userKey, 30); //save the uID into the cookie
-  startgame(); //reset the game
+  startGame(); //reset the game
 
 }
 //Google Sign out function
