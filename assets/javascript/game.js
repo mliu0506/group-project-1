59aa0aba88d9fa39926e996de3423a0153b3508d
S Arashi <34376591+AstraySteve@users.noreply.github.com>
2018-06-01 18:07:16 -0400

    disabled firebase reference for testing game.js, added local chat, RPS logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,45 @@
-/*Functions for webcam and emotion detect*/
 /*
-    TODO STEVEN:
-    1) Plan out flow of data
-    2) GamePage.html and GamePage Chat
+    game.js contains the JavaScript to run the main game of Rock Paper Scissor (Happy, Suprise, Neutral)
+    Webcam library and Face++ API are called here
 */
-//TODO FIREBASE LISTENER HERE
 
+//GamePage Global Variables
+var messageList = $("#messageList"); //Variable for local chat messages
+
+/* Disabled firebase listener for testing
+//TODO FIREBASE LISTENER HERE
+//TODO fix path listener
+gamesRef.child(gameID).on('value', function(snapshot){
+    //Detect if P1 and P2 exists
+    //NOTE: Change gameRoom so when P2 Joins change state of game to JOINED, Then check here
+    if (snapshot.val().state === "JOINED"){
+        console.log("hi hi");
+        //Start game routine
+    }
+    //Change so to detect player 2 slot open
+    else if(!(snapshot.child(gameID).child('players').child('p2').exists())){
+        console.log(snapshot.val());
+        console.log("nothing here");
+        gamesRef.child(gameID).update({
+            state: "OPEN"
+        });
+    }
+});
+
+//Listen event for local chat messages
+gamesRef.child(gameID).child('chat').on('child_added', function(chatsnapshot){
+    if (messageList.text() == ""){
+        //Account for the first blank entry
+        messageList.text(`${chatsnapshot.val().playerName}: ${chatsnapshot.val().message}`);
+    }
+    else{
+        var previousText = messageList.text();
+        messageList.text(`${previousText}\n${chatsnapshot.val().playerName}: ${chatsnapshot.val().message}`);
+    }
+    //auto scroll to bottom of textarea, show latest chat
+    messageList.scrollTop(messageList[0].scrollHeight);
+});
+*/
 //Active and attach camera to DOM element
 //Webcam.attach('#my_camera');
 
@@ -19,6 +53,7 @@ function take_snapshot(){
     image data will be passed as data_uri*/
     Webcam.snap(function(data_uri){
         detectFace(data_uri);
+        console.log("picture taken");
     });
     //Turns off Camera
     //Webcam.reset();
@@ -59,37 +94,103 @@ function detectFace(data_uri){
             //Face++ detected a face, start analying emotions
             var emotions = response.faces[0].attributes.emotion;
             var emotionValue = Math.max(emotions.happiness,emotions.surprise,emotions.neutral);
+            var likely = likelyEmotion(emotionValue);
             var emotion;
             //compare most likely emotion
             switch (emotionValue){
                 case emotions.happiness:
                     emotion = "Happy";
-                    console.log("you are happy");
+                    //console.log("you are happy");
                     break;
                 case emotions.surprise:
                     emotion = "Surprise";
-                    console.log("you are suprised");
+                    //console.log("you are suprised");
                     break;
                 case emotions.neutral:
                     emotion = "Neutral";
-                    console.log("you are neutral");
+                   // console.log("you are neutral");
             }
-
             //TODO SEND TO FIREBASE update player data
+            var playerData = {
+                emotion: emotion,
+                likely: likely
+            }
+            //FirebasePath.update(playerData);
 
             //DEBUG LOG
-            //console.log("face detected");
+            /*console.log("face detected");
+            console.log(likely);
             console.log("Happiness: " + emotions.happiness);
             console.log("Surprise: " + emotions.surprise);
-            console.log("Neutral: " + emotions.neutral)
+            console.log("Neutral: " + emotions.neutral);*/
         }
     });
 }
 
-/*TODO RPS LOGIC HERE AND UPDATE FIREBASE*/
+function likelyEmotion(value){
+    //Function takes an int value and returns a string response depending on likely emotion
+    if (value >= 70){
+        return "Very likely";
+    }
+    else if (value < 30){
+        return "Best match";
+    }
+    else{
+        return "Likely";
+    }
+}
 
-//RPS logic: Happy > Neutral, Neutral > Suprise, Suprise > Happy
-//NOTE: Global variable works
-/*gamesRef.set({
-    gameID: "Game1",
-});*/
+/*TODO RPS LOGIC HERE AND UPDATE FIREBASE*/
+function compareFace(playerChoice, opponentChoice){
+    //Function to compare player's choice with opponent's choice, return string: win, lose or draw
+    //RPS logic: Happy > Neutral, Neutral > Suprise, Suprise > Happy
+    switch(playerChoice){
+        case 'Happy':
+            switch(opponentChoice){
+                case 'Neutral':
+                    return 'win';
+                case 'Suprise':
+                    return 'lose';
+            }
+            break;
+        case 'Neutral':
+            switch(opponentChoice){
+                case 'Suprise':
+                    return 'win';
+                case 'Happy':
+                    return 'lose';
+            }
+            break;
+        case 'Suprise':
+            switch(opponentChoice){
+                case 'Happy':
+                    return 'win';
+                case 'Neutral':
+                    return 'lose';
+            }
+            break;
+        default:
+            return 'draw';
+    }
+}
+
+function sendChatMessage(message){
+    //Function to send local game messages to database
+    var ref = gamesRef.child(gameID).child('chat');
+    ref.push().set({
+        playerName: playerName,
+        message: message
+    });
+}
+
+//Shorthand for $(document).ready(function(){...});
+$(function(){
+    //Click event for local message field submit
+    $("#submitMessage").on("click", function(event){
+        event.preventDefault();
+        var message = $("#message").val();
+        sendChatMessage(message);
+        //Clear chat fields
+        $("#message").val("");
+    });
+});
