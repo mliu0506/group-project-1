719007edbdec801ca43dfcf85b86a74a4aa57707
Michael Liu <michaelliu56@gmail.com>
2018-05-31 22:37:12 -0400

    modify files

--- assets/javascript/gameroom.js
+++ assets/javascript/gameroom.js
@@ -4,7 +4,7 @@
       console.log("Game List :" + userKey);
       $(".gameroom").empty();
       childsnapshot.forEach(function(child) {
-        if (child.key !== userKey){
+       // if (child.key !== userKey){
           
           var photo = child.val().photo;
           var name = child.val().name;
@@ -15,7 +15,7 @@
           if (status ==="pending") {
             $(".gameroom").append("<div class='chat'><div class='chat-header clearfix'><img class='rounded-circle' src="+ photo +" alt='avatar' /><div class='chat-about'><div class='chat-with'> Game created by " + name + "</div><span class='message-data-time'>" + timestamp + "</span> &nbsp; &nbsp;<button class='joingame'>Join Game</button></div></div><i class='fa fa-star'></i></div>");
           }
-        }
+       // }
       });
     });
   }
