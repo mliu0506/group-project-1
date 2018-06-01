7994124546bc4c78df87ff59e2cd4aa3901b9961
Michael Liu <michaelliu56@gmail.com>
2018-06-01 00:31:24 -0400

    modify files

--- assets/javascript/gameroom.js
+++ assets/javascript/gameroom.js
@@ -13,7 +13,7 @@
           console.log("Game Name:" + child.val().name);
           console.log("Game Status:" + child.val().status);
           if (status ==="pending") {
-            $(".gameroom").append("<BR><div class='chat'><div class='chat-header clearfix'><img class='rounded-circle' src="+ photo +" alt='avatar' /><div class='chat-about'><div class='chat-with'> Game created by " + name + "</div><span class='message-data-time'>" + timestamp + "</span></div> <button class='joingame' data-value='"+ gameID + "'>Join Game</button><i class='fa fa-star'></i></div></div>");
+            $(".gameroom").append("<BR><div class='chat'><div class='chat-header clearfix'><img class='rounded-circle' src="+ photo +" alt='avatar' /><div class='chat-about'><div class='chat-with'> Game created by " + name + "</div><span class='message-data-time'>" + timestamp + "</span></div><button id='joingame' data-value='"+ gameID + "'>Join Game</button><i class='fa fa-star'></i></div></div>");
             $(".gameroom").append("<BR>");
           }
        // }
@@ -23,8 +23,8 @@
 
 
 function joinGame() {
-  $(".joingame").on("click",function() {
-    var gameID = $(".joingame").attr("data-value");
+  $("#joingame").on("click",function() {
+    var gameID = $("#joingame").attr("data-value");
     var d = new Date();
     var timestamp = d.toUTCString();
     console.log("Joined Game : "+ gameID);
@@ -32,7 +32,7 @@ function joinGame() {
       gamesRef.child(gameID).update({status:'matched',timestamp:timestamp});
       gamesRef.child(gameID).child("players").child("player1").update({status:'matched_players2'});
       gamesRef.child(gameID).child("players").child("player2").update({uID:userKey,win:0,lose:0,name:name,status:'matched_player2'});
-      $(".joingame").hide();
+      $("#joingame").hide();
     }
   });
 }
