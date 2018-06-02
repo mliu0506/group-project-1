498712a3f16be80bc2720b5491c3161078d063a1
Michael Liu <michaelliu56@gmail.com>
2018-06-01 22:33:49 -0400

    modify files

--- assets/javascript/gameroom.js
+++ assets/javascript/gameroom.js
@@ -13,8 +13,8 @@
           console.log("Game Name:" + child.val().name);
           console.log("Game Status:" + child.val().status);
           if (status ==="pending") {
-            $(".gameroom").append("<BR><div class='chat'><div class='chat-header clearfix'><img class='rounded-circle' src="+ photo +" alt='avatar' /><div class='chat-about'><div class='chat-with'> Game created by " + name + "</div><span class='message-data-time'>" + timestamp + "</span></div><button class='join-game' data-value='"+ gameID + "'>Join Game</button><i class='fa fa-star'></i></div></div>");
-            $(".gameroom").append("<BR>");
+            $(".gameroom").append("<div class='chat'><div class='chat-header clearfix'><img class='rounded-circle' src="+ photo +" alt='avatar' /><div class='chat-about'><div class='chat-with'> Game created by " + name + "</div><span class='message-data-time'>" + timestamp + "</span></div><button class='join-game' data-value='"+ gameID + "'>Join Game</button><i class='fa fa-star'></i></div></div>");
+          //  $(".gameroom").append("<BR>");
           }
        // }
       });
